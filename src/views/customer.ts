4aa57f120d2589628bced190619f62e8906c3a3c
dhodi-fahad <dhodifahad@gmail.com>
2023-04-09 17:18:00 +0300

    added class for add new customer page object

---
+++ src/views/customer.ts
@@ -0,0 +1,115 @@
+/// <reference types="cypress" />
+
+
+/**
+ * Represents a page object for adding a new customer.
+ * Provides methods for interacting with the input fields and buttons
+ * of the add customer form.
+ *
+ * @class AddCustomer
+ */
+export class AddCustomer {
+
+  url = 'addcustomer'
+
+  /**
+   * Visits the Url to  Add Customer page
+   * @return {this}  object instance of CreateInvoice class.
+   */
+  visit(){
+    cy.visit(this.url)
+    cy.wait(1000)
+    return this
+  }
+
+  /**
+   * Selects the customer category from the dropdown
+   * with data-testid "customer_category".
+   *
+   * @param {string} category - The customer category to select.
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  selectCustomerCategory = (category = 'Walk in customer'): AddCustomer => {
+    cy.get('[data-testid="customer_category"]')
+      .find('input')
+      .clear()
+      .type(`${category} {enter}`, { force: true });
+    cy.wait(500);
+    return this;
+  };
+  /**
+   * Enters the customer name into the input field
+   * with data-testid "customer_name".
+   *
+   * @param {string} name - The customer's name.
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  enterCustomerName = (name: string): AddCustomer => {
+    cy.get('[data-testid="customer_name"]').find('input').clear().type(name);
+    cy.wait(500);
+    return this;
+  };
+
+  /**
+   * Enters the customer mobile number into the input field
+   * with data-testid "customer_mobile".
+   *
+   * @param {string} mobile - The customer's mobile number.
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  enterCustomerMobile = (mobile: string): AddCustomer => {
+    cy.get('[data-testid="customer_mobile"]').find('input').clear().type(mobile);
+    cy.wait(500);
+    return this;
+  };
+
+  /**
+   * Enters the customer email into the input field
+   * with data-testid "customer_email".
+   *
+   * @param {string} email - The customer's email address.
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  enterCustomerEmail = (email: string): AddCustomer => {
+    cy.get('[data-testid="customer_email"]').find('input').clear().type(email);
+    cy.wait(500);
+    return this;
+  };
+
+  /**
+   * Enters the customer address into the input field
+   * with data-testid "customer_address".
+   *
+   * @param {string} address - The customer's address.
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  enterCustomerAddress = (address: string): AddCustomer => {
+    cy.get('[data-testid="customer_address"]').find('input').clear().type(address);
+    cy.wait(500);
+    return this;
+  };
+
+  /**
+   * Clicks the "Save" button with data-testid "save_customer".
+   *
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  clickSaveCustomer = (): AddCustomer => {
+    cy.get('[data-testid="save_customer"]').click();
+    cy.wait(500);
+    return this;
+  };
+
+  /**
+   * Enters the customer credit limit into the input field
+   * with data-testid "credit_limit".
+   *
+   * @param {string} limit - The customer's credit limit.
+   * @returns {AddCustomer} - The instance of the AddCustomer class.
+   */
+  enterCreditLimit = (limit: string): AddCustomer => {
+    cy.get('[data-testid="credit_limit"]').find('input').clear().type(limit);
+    cy.wait(500);
+    return this;
+  };
+}
